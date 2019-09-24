539dc7d43897047700b4b13cf6304c48d0336d70
Jesper larsen <zuqle@hotmail.com>
2019-09-24 13:26:31 +0200

    sql update

--- content/class/db.php
+++ content/class/db.php
@@ -1,1 +1,28 @@
-//This is the sql query selector
+<?php
+class DB {
+    private static function connect () {
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=ajax_test;charset=utf8', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+
+    public static function query($query, $param = array()) {
+        $statement = self::connect()->prepare($query);
+        $statement->execute($param);
+
+        if (explode(' ', $query)[0] == 'SELECT') {
+            $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+            return $data;
+        }
+    }
+}
+
+
+
+
+$message = DB::query("SELECT * FROM messenger");
+
+foreach($message as $value){
+    //Print the element out.
+    echo ''.$value['message'].'';
+}
